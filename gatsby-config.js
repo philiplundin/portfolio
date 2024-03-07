df9bfe01cb35b4630d8475d0252855c98c1aaacb
Philip Lundin <lundiin@live.se>
2024-03-07 17:43:49 +0100

    chore: add gatsby svg library & typescript svg config

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,5 +40,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/
+        },
+      },
+    },
   ],
 }
