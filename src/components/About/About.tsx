c49a8df6893e4dfe9b06fd3e60aadde5bf6eeb51
Philip Lundin <lundiin@live.se>
2024-03-07 14:57:45 +0100

    refactor: classname change

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,7 +4,7 @@ import './About.css'
 
 const About: React.FC = () => {
     return (
-        <div className='container'>
+        <div className='about'>
             <h3>About</h3>
             <p>Donec semper blandit mi, eu rutrum lorem commodo vitae. Donec sagittis imperdiet vulputate. Vestibulum rhoncus ipsum non commodo pretium. Aenean molestie a ipsum in maximus. In rhoncusquis augue vel volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus posuere dolor sit amet viverra consectetur. Sed facilisis tincidunt consectetur. Maecenas id eros lacinia libero fringilla convallis.</p>
         </div>
