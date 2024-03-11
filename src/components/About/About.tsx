f544794205006f5edf13b010d4c9dcfd9556ca7e
Philip Lundin <lundiin@live.se>
2024-03-11 22:57:55 +0100

    refactor: moved about text to json. updated placeholder texts. style: changed spacing in all sections

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,13 +1,13 @@
 import * as React from "react"
 import './About.css'
-
+import JSONData from '../../assets/About-text.json';
 
 const About: React.FC = () => {
     return (
         <section id='about' className='about'>
             <h3 className='about-heading'>ABOUT</h3>
-            <p className='about-text'>Donec semper blandit mi, eu rutrum lorem commodo vitae. Donec sagittis imperdiet vulputate. Vestibulum rhoncus ipsum non commodo pretium. Aenean molestie a ipsum in maximus. In rhoncusquis augue vel volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus posuere dolor sit amet viverra consectetur. Sed facilisis tincidunt consectetur. Maecenas id eros lacinia libero fringilla convallis.</p>
-            <p className='about-text'><br/>Donec semper blandit mi, eu rutrum lorem commodo vitae. Donec sagittis imperdiet vulputate. Vestibulum rhoncus ipsum non commodo pretium. Aenean molestie a ipsum in maximus. In rhoncusquis augue vel volutpat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus posuere dolor sit amet viverra consectetur. Sed facilisis tincidunt consectetur. Maecenas id eros lacinia libero fringilla convallis.</p>
+            <p className='about-text'>{JSONData.paragraph1}</p>
+            <p className='about-text'>{JSONData.paragraph2}</p>
         </section>
     )
 }
