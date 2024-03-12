b5c84a60f5ec9153c5aec9defc06ac7b7382b277
Philip Lundin <lundiin@live.se>
2024-03-12 17:24:32 +0100

    fix: preventing fontawesome to flash with incorrect css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Header from "../components/Header"
 import Content from "../components/Content"
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 const IndexPage: React.FC = () => (
   <Layout>
