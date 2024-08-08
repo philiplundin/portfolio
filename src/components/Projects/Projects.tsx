f180b40e7493a5c550a3393ce5305b62b218ee4b
Philip Lundin <lundiin@live.se>
2024-08-08 13:22:35 +0200

    fix: updated texts

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -11,15 +11,6 @@ const Projects: React.FC = () => {
         <section className='content-spacing' id='projects'>
             <h3 className='content-heading'>PROJECTS</h3>
             <ul className='projects-list'>
-                <a href='https://philiplundin.github.io/body-mind/' target='_blank'>
-                    <li className='projects-list-item'>
-                        <img className='projects-image' src={BodyMind} alt='body-mind' />
-                        <span className='projects-text-box'>
-                            <h3 className='projects-heading-small'>Body & Mind</h3>
-                            <p className='projects-text'>React website for a company offering adventures. It's a work in progress.</p>
-                        </span>
-                    </li>
-                </a>
                 <a href='https://play.google.com/store/apps/details?id=com.staccsandroidtv' target='_blank'>
                     <li className='projects-list-item'>
                         <img className='projects-image' src={AndroidTV} alt='android-tv' />
@@ -35,6 +26,15 @@ const Projects: React.FC = () => {
                         <span className='projects-text-box'>
                             <h3 className='projects-heading-small'>Staccs - Apple TV</h3>
                             <p className='projects-text'>React native application built for Staccs users with a paid subscription.</p>
+                        </span>
+                    </li>
+                </a>
+                <a href='https://philiplundin.github.io/body-mind/' target='_blank'>
+                    <li className='projects-list-item'>
+                        <img className='projects-image' src={BodyMind} alt='body-mind' />
+                        <span className='projects-text-box'>
+                            <h3 className='projects-heading-small'>Body & Mind</h3>
+                            <p className='projects-text'>React application for a upcoming company offering hikes. It's a work in progress.</p>
                         </span>
                     </li>
                 </a>
