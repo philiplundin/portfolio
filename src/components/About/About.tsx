e9fcd9af87bd0119f6acf53fe7c905b4877cab51
Philip Lundin <lundiin@live.se>
2024-08-16 13:19:52 +0200

    fix: updated text

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,7 +5,7 @@ import JSONData from '../../assets/About-text.json';
 const About: React.FC = () => {
     return (
         <section id='about' className='about'>
-            <h3 className='about-heading'>ABOUT</h3>
+            <h3 className='about-heading'>ABOUT ME</h3>
             <p className='about-text'>{JSONData.paragraph1}</p>
             <p className='about-text'>{JSONData.paragraph2}</p>
         </section>
